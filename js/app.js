038bc0378598dd70ecbc9c314a2bd241b83a30f0
Rushan <xamidullin.rushan@gmail.com>
2025-05-05 17:10:21 +0300

    0.11

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-let button = document.getElementsByClassName("burger__menu")
+const menuButton = document.querySelector('.burger__menu');
+const menu = document.querySelector('.navigation__menu');
+
+menuButton.addEventListener('click', ()=> {
+    menu.classList.toggle('menu--open');
+} )
